d7cc8a7eed1274acdbf1aef4f6a05516f86e3c03
konb-hui <2581881495@qq.com>
2019-08-28 14:40:15 +0800

    springboot jsp

--- src/main/java/com/zph/springboot/web/HelloController.java
+++ src/main/java/com/zph/springboot/web/HelloController.java
@@ -1,16 +1,22 @@
 package com.zph.springboot.web;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*接着创建控制器类HelloController， 这个类就是Spring MVC里的一个普通的控制器。
-@RestController 是spring4里的新注解，是@ResponseBody和@Controller的缩写*/
-@RestController
+/*修改HelloController，把本来的@RestController 改为@Controller。
+这时返回"hello"就不再是字符串，而是根据application.properties 中的视图重定向，到/WEB-INF/jsp目录下去寻找hello.jsp文件*/
+@Controller
 public class HelloController {
 	
 	@RequestMapping("/hello")
-	public String hello() {
-		return "Hello Spring Boot!";
+	public String hello(Model m) {
+		m.addAttribute("now", DateFormat.getDateInstance().format(new Date()));
+		return "hello";
 	}
 	
 }
