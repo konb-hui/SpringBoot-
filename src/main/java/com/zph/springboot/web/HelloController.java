8d54a0f8cfce5ba7ffc8258b94d42876e9a539e6
konb-hui <2581881495@qq.com>
2019-08-28 14:58:33 +0800

    springboot 错误处理

--- src/main/java/com/zph/springboot/web/HelloController.java
+++ src/main/java/com/zph/springboot/web/HelloController.java
@@ -8,14 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*修改HelloController，把本来的@RestController 改为@Controller。
-这时返回"hello"就不再是字符串，而是根据application.properties 中的视图重定向，到/WEB-INF/jsp目录下去寻找hello.jsp文件*/
+/*修改HelloController，使得访问/hello一定会产生异常: some exception */
 @Controller
 public class HelloController {
 	
 	@RequestMapping("/hello")
-	public String hello(Model m) {
+	public String hello(Model m) throws Exception{
 		m.addAttribute("now", DateFormat.getDateInstance().format(new Date()));
+		if(true) {
+			throw new Exception("some exception");
+		}
 		return "hello";
 	}
 	
