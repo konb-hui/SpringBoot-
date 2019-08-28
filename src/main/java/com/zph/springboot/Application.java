edeebff0bb8671eea40ddb84b9b0f09ff966b2bd
konb-hui <2581881495@qq.com>
2019-08-28 14:14:19 +0800

    springboot部署-war方式

--- src/main/java/com/zph/springboot/Application.java
+++ src/main/java/com/zph/springboot/Application.java
@@ -2,11 +2,21 @@ package com.zph.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 //创建 Application.java，其注解 @SpringBootApplication 
 //表示这是一个SpringBoot应用，运行其主方法就会启动tomcat,默认端口是8080
 @SpringBootApplication
-public class Application {
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
