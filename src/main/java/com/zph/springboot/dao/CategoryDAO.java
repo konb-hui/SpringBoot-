0fca15790b109d0441ffaa2a1736af49b89308bd
konb-hui <2581881495@qq.com>
2019-08-29 14:45:41 +0800

    springboot CRUD+分页 jpa

--- src/main/java/com/zph/springboot/dao/CategoryDAO.java
+++ src/main/java/com/zph/springboot/dao/CategoryDAO.java
@@ -1,10 +1,10 @@
-//package com.zph.springboot.dao;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.zph.springboot.pojo.Category;
-//
-////JpaRepository 这个父接口，就提供了CRUD, 分页等等一系列的查询了，直接拿来用，都不需要二次开发的了。
-//public interface CategoryDAO extends JpaRepository<Category, Integer> {
-//
-//}
+package com.zph.springboot.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zph.springboot.pojo.Category;
+
+//JpaRepository 这个父接口，就提供了CRUD, 分页等等一系列的查询了，直接拿来用，都不需要二次开发的了。
+public interface CategoryDAO extends JpaRepository<Category, Integer> {
+
+}
--- src/main/java/com/zph/springboot/web/CategoryController.java
+++ src/main/java/com/zph/springboot/web/CategoryController.java
@@ -3,25 +3,63 @@ package com.zph.springboot.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//import com.zph.springboot.dao.CategoryDAO;
+import com.zph.springboot.dao.CategoryDAO;
 import com.zph.springboot.mapper.CategoryMapper;
 import com.zph.springboot.pojo.Category;
 
 @Controller
 public class CategoryController {
 	@Autowired
-	CategoryMapper categoryMapper;
+	CategoryDAO categoryDAO;
 	
 	@RequestMapping("/listCategory")
-	public String listCategory(Model m) throws Exception{
-		List<Category> cs = categoryMapper.findAll();
+	public String listCategory(Model m,@RequestParam(value = "start",defaultValue = "0") int start,@RequestParam(value = "size" ,defaultValue = "5") int size) throws Exception{
+		start = start<0?0:start;
+		Sort sort = new Sort(Sort.Direction.DESC,"id");
+		Pageable pageable = new PageRequest(start, size, sort);
+		Page<Category> page = categoryDAO.findAll(pageable);
 		
-		m.addAttribute("cs", cs);
+		System.out.println(page.getNumber());
+		System.out.println(page.getNumberOfElements());
+		System.out.println(page.getSize());
+		System.out.println(page.getTotalElements());
+		System.out.println(page.getTotalPages());
 		
+		m.addAttribute("page",page);
 		return "listCategory";
+	}
+	
+	@RequestMapping("/addCategory")
+	public String addCategory(Category c) throws Exception{
+		categoryDAO.save(c);
+		return "redirect:listCategory";
+	}
+	
+	@RequestMapping("updateCategory")
+	public String updateCategory(Category c) throws Exception{
+		categoryDAO.save(c);
+		return "redirect:listCategory";
+	}
+	
+	@RequestMapping("editCategory")
+	public String editCategory(int id,Model m) throws Exception{
+		Category c = categoryDAO.getOne(id);
+		m.addAttribute("c", c);
+		return "editCategory";
+	}
+	
+	@RequestMapping("deleteCategory")
+	public String deleteCategory(Category c) throws Exception{
+		categoryDAO.delete(c);
+		return "redirect:listCategory";
 	}
 }
