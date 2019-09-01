68d2a6856c78e0ecedf4ee6e626098f3f0d9b7c9
konb-hui <2581881495@qq.com>
2019-09-01 14:20:59 +0800

    spring boot 表达式 thymeleaf

---
+++ src/main/java/com/zph/springboot/web/TestController.java
@@ -0,0 +1,23 @@
+package com.zph.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zph.springboot.pojo.Product;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String test(Model m) {
+		String htmlContent = "<p style='color:red'>红色文字</p>";
+		Product currentProduct = new Product(5, "product e", 200);
+		
+		m.addAttribute("htmlContent", htmlContent);
+		m.addAttribute("currentProduct",currentProduct);
+		
+		return "test";
+	}
+	
+}
