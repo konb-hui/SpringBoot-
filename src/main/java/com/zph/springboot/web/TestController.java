b4f119a655bfb0422d7d8492660a1ef9bba1b8ad
konb-hui <2581881495@qq.com>
2019-09-02 14:10:27 +0800

    spring boot 条件 thymeleaf

--- src/main/java/com/zph/springboot/web/TestController.java
+++ src/main/java/com/zph/springboot/web/TestController.java
@@ -13,9 +13,11 @@ public class TestController {
 	public String test(Model m) {
 		String htmlContent = "<p style='color:red'>红色文字</p>";
 		Product currentProduct = new Product(5, "product e", 200);
+		boolean testBoolean = false;
 		
 		m.addAttribute("htmlContent", htmlContent);
 		m.addAttribute("currentProduct",currentProduct);
+		m.addAttribute("testBoolean", testBoolean);
 		
 		return "test";
 	}
