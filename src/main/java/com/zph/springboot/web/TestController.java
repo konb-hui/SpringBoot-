3d0244ccaee8e89a65cee5752517c575d148fb53
konb-hui <2581881495@qq.com>
2019-09-02 14:34:48 +0800

    spring boot 遍历 thymeleaf

--- src/main/java/com/zph/springboot/web/TestController.java
+++ src/main/java/com/zph/springboot/web/TestController.java
@@ -1,5 +1,8 @@
 package com.zph.springboot.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,16 @@ public class TestController {
 		Product currentProduct = new Product(5, "product e", 200);
 		boolean testBoolean = false;
 		
+		List<Product> ps = new ArrayList<Product>();
+		ps.add(new Product(1, "product a", 50));
+		ps.add(new Product(2, "product b", 100));
+		ps.add(new Product(3, "product c", 150));
+		ps.add(new Product(4, "product d", 200));
+		ps.add(currentProduct);
+		ps.add(new Product(6, "product f", 200));
+		ps.add(new Product(7, "product g", 200));
+		
+		m.addAttribute("ps", ps);
 		m.addAttribute("htmlContent", htmlContent);
 		m.addAttribute("currentProduct",currentProduct);
 		m.addAttribute("testBoolean", testBoolean);
